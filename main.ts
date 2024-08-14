c391e6094fc1f7ba3265fd93b782fdfe3b375448
TheWooffles <95188174+TheWooffles@users.noreply.github.com>
2024-08-14 01:18:37 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+player.onTellCommand("Help", function () {
+    player.tell(mobs.target(LOCAL_PLAYER), "Signal Client - v.1 Commands")
+    player.tell(mobs.target(LOCAL_PLAYER), "<1- Help> List All Commands")
+    player.tell(mobs.target(LOCAL_PLAYER), "<2- Atp> Agent Teleport")
+})
 player.tell(mobs.target(LOCAL_PLAYER), "Signal Client - v.1 Starting...")
 loops.pause(1000)
 player.tell(mobs.target(LOCAL_PLAYER), "Signal Client - v.1 Initialized>>>")
